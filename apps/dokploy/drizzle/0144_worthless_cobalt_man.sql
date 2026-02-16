b504f984f55844e02696f2b90e54446cea180dae
AlexDev404 <34188635+AlexDev404@users.noreply.github.com>
2026-02-16 01:29:43 -0600

    fix(postgres): finish fixing migrations

--- apps/dokploy/drizzle/0144_worthless_cobalt_man.sql
+++ apps/dokploy/drizzle/0144_worthless_cobalt_man.sql
@@ -1,4 +1,3 @@
-ALTER TABLE "sso_provider" ADD COLUMN "created_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint
 ALTER TABLE "webServerSettings" ADD COLUMN "whitelabelLogoUrl" text;--> statement-breakpoint
 ALTER TABLE "webServerSettings" ADD COLUMN "whitelabelBrandName" text;--> statement-breakpoint
 ALTER TABLE "webServerSettings" ADD COLUMN "whitelabelTagline" text;
